dec7f14a7ee8d2bd2e25315716d6df3142fe8633
DeepuSharma26 <deepasharmaa37@gmail.com>
2021-01-15 13:46:06 +0530

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,26 @@
+from convert_units import UnitConversion
+
+
+class UserInput:
+    def __init__(self):
+        pass
+    convert = UnitConversion()
+
+    def _get_user_input(self):
+        value = self._get_user_input()
+        vals = json.loads(value)
+    
+    def getGender(self):
+        gender = vals['gender']
+        return gender
+        
+    def getWeight(self):
+        weight = vals['Weight']
+        return weight
+    
+    def getHeight(self):
+        Height = vals['Height']
+        return height
+     
+    
+     
